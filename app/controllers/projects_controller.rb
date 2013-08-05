0dddf8fcb4aaabfe674bcd22f447b84d7ecaf34c
yarfreese <yarfreese@gmail.com>
2013-08-05 16:38:00 -0700

    Restrict access to project actions to admins only.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 
+  before_action :authorize_admin!, except: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -46,6 +47,17 @@ class ProjectsController < ApplicationController
 
     redirect_to projects_path
   end
+
+  private
+ 
+    def authorize_admin!
+      require_signin!
+
+      unless current_user.admin?
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to root_path
+      end
+    end
 
   private
     def project_params
